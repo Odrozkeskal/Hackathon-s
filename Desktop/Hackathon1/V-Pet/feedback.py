e1d1b5c0d47e78ec0679c8e21b283a664489df8c
DiGureev <dianagureev@gmail.com>
2024-07-07 16:04:09 +0300

    feedback for Hackathon 1

---
+++ Desktop/Hackathon1/V-Pet/feedback.py
@@ -0,0 +1,11 @@
+The project works, made with PyQt5. Looks nice! I enjoyed playing :) You have practised OOP and modules. Your code is clean enough, I can see you put a lot of effort and the project is very good. 
+Ideas for improvement:
+- I see you have not practised DB here in the project. You can add, like creating your own animal, so you can play with that and save instance in the table for example :) 
+
+But in general I love the project! 
+
+Python OOP: 10/10
+Modules: 10/10
+Database: 0/10
+
+Overall: 7
